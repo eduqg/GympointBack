56327719d6eafc8db198a6e45c00ff28a8c9001a
eduqg <eduardoqgomes@gmail.com>
2019-10-17 11:34:10 -0300

    Posting first user with controller

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,12 @@
 /* Responsável por rotas que serão utlizadas em todo projeto */
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-// Requisiçaõ de teste para criar primeiro usuário
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Dudu',
-    email: 'dudu@edu.com',
-    password_hash: '821812',
-    admin: true,
-  });
-
-  return res.json(user);
-});
+// Cadastro de Usuário
+routes.post('/users', UserController.store);
 
 // Será utilizado na classe App, para configurar rotas disponíveis
 export default routes;
